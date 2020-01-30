0a3292197db027d6af3494009675a41487b7afa6
BroSmithBoris <47716016+BroSmithBoris@users.noreply.github.com>
2020-01-30 17:42:10 +0500

    ET_0.0.3
    Добавил управление для камеры

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    KeyCode up = KeyCode.W;
-    KeyCode down = KeyCode.S;
-    KeyCode left = KeyCode.A;
-    KeyCode right = KeyCode.D;
+    KeyCode Right = KeyCode.D;
+    KeyCode Left = KeyCode.A;
+    KeyCode Up = KeyCode.Q; 
+    KeyCode Down = KeyCode.E;
+    KeyCode Forward = KeyCode.W;
+    KeyCode Back = KeyCode.S;
+    private Vector3 moveDirectionLocal = Vector3.zero;
     private Vector3 moveDirection = Vector3.zero;
     public float speed;
 
@@ -15,25 +18,36 @@ public class CameraMove : MonoBehaviour
     void Update()
     {
         int x;
+        int yz;
         int z;
 
-        if (Input.GetKey(up))
-            x = 1;
-        else if (Input.GetKey(down))
+        if (Input.GetKey(Left))
             x = -1;
+        else if (Input.GetKey(Right))
+            x = 1;
         else
             x = 0;
 
-        if (Input.GetKey(left))
-            z = -1;
-        else if (Input.GetKey(right))
+        if (Input.GetKey(Up))
+            yz = -1;
+        else if (Input.GetKey(Down))
+            yz = 1;
+        else
+            yz = 0;
+
+        if (Input.GetKey(Forward))
             z = 1;
+        else if (Input.GetKey(Back))
+            z = -1;
         else
             z = 0;
 
+        moveDirectionLocal = new Vector3(0, 0, yz);
         moveDirection = new Vector3(x, 0, z);
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection = moveDirection * speed;
+
+        moveDirectionLocal = transform.TransformDirection(moveDirectionLocal);
+        moveDirection = (moveDirection + moveDirectionLocal) * speed;
+        transform.position = transform.position + moveDirection * Time.deltaTime;
 
     }
 }
