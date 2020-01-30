89e91e0fbcb789062ce75a37a87560d14a29a0bf
Egorka0708 <egortush@yandex.ru>
2020-01-30 16:59:52 +0500

    ET_0.0.2
    нерабочий скрипт движения мышки

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    KeyCode up = KeyCode.W;
+    KeyCode down = KeyCode.S;
+    KeyCode left = KeyCode.A;
+    KeyCode right = KeyCode.D;
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x;
+        int z;
+
+        if (Input.GetKey(up))
+            x = 1;
+        else if (Input.GetKey(down))
+            x = -1;
+        else
+            x = 0;
+
+        if (Input.GetKey(left))
+            z = -1;
+        else if (Input.GetKey(right))
+            z = 1;
+        else
+            z = 0;
+
+        moveDirection = new Vector3(x, 0, z);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = moveDirection * speed;
+
+    }
+}
