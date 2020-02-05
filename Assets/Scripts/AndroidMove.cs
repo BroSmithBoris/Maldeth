3839ef4256f0540635de9b4251a235d1646ea3fe
Egorka0708 <egortush@yandex.ru>
2020-02-06 02:31:46 +0500

    GT_0.0.1
    Залил тестовую карту и перемещение камеры по ней с помощью свайпов по экрану (или мышкой, что аналогично)

---
+++ Assets/Scripts/AndroidMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AndroidMove : MonoBehaviour
+{
+    private Vector3 startPos;
+    public Camera cam;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            startPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        else if (Input.GetMouseButton(0))
+        {
+            float posX = cam.ScreenToViewportPoint(Input.mousePosition).x - startPos.x;
+            float posZ = cam.ScreenToViewportPoint(Input.mousePosition).y - startPos.y;
+            transform.position = new Vector3(transform.position.x - posX, transform.position.y, transform.position.z - posZ);
+        }
+    }
+}
--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using cakeslice;
-
 public class HexCell : MonoBehaviour
 {
     public float x, z; //координаты гекса
